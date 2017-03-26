505a4c8794f05ed0ce40dc0788241066c3ee544a
Harshit Tewari <hat20@users.noreply.github.com>
2017-03-27 02:18:13 +0530

    Create helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,11 @@
+//Printing Hello World 100 times
+Class HelloWorld
+{
+    public static void main()
+    {
+        for(int i = 0; i<100; i++)
+        { 
+            System.out.println("Hello World");
+        }
+    }
+}    
